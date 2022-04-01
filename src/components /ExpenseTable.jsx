e0d53687fec5c9e341d1bf60bc6ec6433586939c
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-04-01 17:29:18 -0300

    Adiciona o requisito 7

---
+++ src/components /ExpenseTable.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ExpenseTable extends Component {
+  render() { 
+    return ();
+  }
+}
+ 
+export default ExpenseTable;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -31,9 +31,34 @@ class Wallet extends React.Component {
           </span>
           <span data-testid="header-currency-field">BRL</span>
         </header>
-        <main>
-          <ExpenseForm />
-        </main>
+        <ExpenseForm />
+        <table style={ { width: '100%' } }>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Centro comercial Moctezuma</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
